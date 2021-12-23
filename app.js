e64e573538f98f548ce0ec6460e51d018b8b25b5
Mekdim <mekdimashebo@gmail.com>
2021-12-23 23:17:49 +0300

    npm run prod

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express()
 const port = process.env.PORT || 5000 // Heroku will need the PORT environment variable
 
-app.use(express.static('unicorn-front/build'));
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'unicorn-front', 'build', 'index.html' ))
-})
+//app.use(express.static('unicorn-front/build'));
+//app.get('*', (req, res)=>{
+    //res.sendFile(path.join(__dirname, 'unicorn-front', 'build', 'index.html' ))
+//})
 
 app.listen(port, () => console.log(`App is live on port ${port}!`))
